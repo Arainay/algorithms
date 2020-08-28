017f6d892a407b95b392af5b51bf473cdcefcd62
arainay <arainay@gmail.com>
2020-08-28 15:41:49 +0800

    Added BST

---
+++ classes/BinarySearchTree.js
@@ -0,0 +1,40 @@
+import { BinarySearchNode } from './BinarySearchNode';
+
+export default class BinarySearchTree {
+  constructor(value) {
+    this.tree = new BinarySearchNode(value);
+  }
+
+  addValue = (value, tree = this.tree) => {
+    if (value > this.tree.value) {
+      if (tree.right) {
+        this.addValue(value, tree.right);
+      } else {
+        tree.right = new BinarySearchNode(value);
+      }
+    } else if (value < this.tree.value) {
+      if (tree.left) {
+        this.addValue(value, tree.left);
+      } else {
+        tree.left = new BinarySearchNode(value);
+      }
+    }
+  };
+
+  getClosestValue = (value, tree = this.tree, closest = Infinity) => {
+    if (!tree) {
+      return closest;
+    }
+
+    if (value === tree.value) {
+      return value;
+    }
+
+    return this.getClosestValue(
+      value,
+      value > tree.value ? tree.right : tree.left,
+      Math.abs(closest - value) >= Math.abs(tree.value - value) ? tree.value : closest
+    );
+  };
+}
+
